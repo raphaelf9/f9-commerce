efccbda20f17c57be2defad636d451308be7c25e
Raphael Faria <raphaelf9@outlook.com>
2023-01-02 10:46:46 -0300

    ManyToMany with association class

--- f9-commerce/src/main/java/com/raphaelf9/f9commerce/entities/Order.java
+++ f9-commerce/src/main/java/com/raphaelf9/f9commerce/entities/Order.java
@@ -3,6 +3,9 @@ package com.raphaelf9.f9commerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -20,6 +23,9 @@ public class Order {
     private User client;
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order(){
 
@@ -71,5 +77,13 @@ public class Order {
 
     public void setPayment(Payment payment) {
         this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts(){
+        return items.stream().map(item -> item.getProduct()).toList();
     }
 }
--- f9-commerce/src/main/java/com/raphaelf9/f9commerce/entities/Product.java
+++ f9-commerce/src/main/java/com/raphaelf9/f9commerce/entities/Product.java
@@ -3,6 +3,7 @@ package com.raphaelf9.f9commerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -24,6 +25,9 @@ public class Product {
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product(){
 
@@ -80,4 +84,13 @@ public class Product {
     public Set<Category> getCategories() {
         return categories;
     }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getOrders(){
+        return items.stream().map(item-> item.getOrder()).toList();
+    }
 }
+
