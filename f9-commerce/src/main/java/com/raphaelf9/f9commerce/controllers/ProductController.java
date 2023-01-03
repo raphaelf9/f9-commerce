adda04d68601c0c3675c171afdde74062b64d824
Raphael Faria <raphaelf9@outlook.com>
2023-01-03 11:27:54 -0300

    Get products paged

--- f9-commerce/src/main/java/com/raphaelf9/f9commerce/controllers/ProductController.java
+++ f9-commerce/src/main/java/com/raphaelf9/f9commerce/controllers/ProductController.java
@@ -1,16 +1,16 @@
 package com.raphaelf9.f9commerce.controllers;
 
 import com.raphaelf9.f9commerce.dto.ProductDTO;
-import com.raphaelf9.f9commerce.entities.Product;
-import com.raphaelf9.f9commerce.repositories.ProductRepository;
 import com.raphaelf9.f9commerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/products")
@@ -22,6 +22,11 @@ public class ProductController {
     public ProductDTO findById(@PathVariable Long id){
        ProductDTO dto = service.findById(id);
        return dto;
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
     }
 
 }
--- f9-commerce/src/main/java/com/raphaelf9/f9commerce/services/ProductService.java
+++ f9-commerce/src/main/java/com/raphaelf9/f9commerce/services/ProductService.java
@@ -4,11 +4,12 @@ import com.raphaelf9.f9commerce.dto.ProductDTO;
 import com.raphaelf9.f9commerce.entities.Product;
 import com.raphaelf9.f9commerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.swing.text.html.Option;
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -20,6 +21,13 @@ public class ProductService {
     public ProductDTO findById(Long id){
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
+
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(item -> new ProductDTO(item));
 
     }
 }
