156f69be351554491f853e228b6d9ed4946ad4ec
Raphael Faria <raphaelf9@outlook.com>
2023-01-04 19:12:38 -0300

    POST new Product - Response Entity

--- f9-commerce/src/main/java/com/raphaelf9/f9commerce/services/ProductService.java
+++ f9-commerce/src/main/java/com/raphaelf9/f9commerce/services/ProductService.java
@@ -30,4 +30,17 @@ public class ProductService {
         return result.map(item -> new ProductDTO(item));
 
     }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto){
+       Product entity = new Product();
+       entity.setName(dto.getName());
+       entity.setDescription(dto.getDescription());
+       entity.setPrice(dto.getPrice());
+       entity.setImgUrl(dto.getImgUrl());
+
+       entity = repository.save(entity);
+       return new ProductDTO(entity);
+
+    }
 }
