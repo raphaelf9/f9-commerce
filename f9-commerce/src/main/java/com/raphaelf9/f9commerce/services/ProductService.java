009e3a7743a7fe4c95e505fcad13d803f6ac506e
Raphael Faria <raphaelf9@outlook.com>
2023-01-04 19:55:42 -0300

    DELETE product

--- f9-commerce/src/main/java/com/raphaelf9/f9commerce/services/ProductService.java
+++ f9-commerce/src/main/java/com/raphaelf9/f9commerce/services/ProductService.java
@@ -49,10 +49,19 @@ public class ProductService {
 
     }
 
+    @Transactional
+    public void delete(Long id){
+        repository.deleteById(id);
+
+    }
+
     private void copyDtoToEntity(ProductDTO dto, Product entity) {
         entity.setName(dto.getName());
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
     }
+
+
+
 }
