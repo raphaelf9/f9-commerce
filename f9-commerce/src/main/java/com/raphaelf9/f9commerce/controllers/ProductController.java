f45c111bb80b5a7e4bd3496c7d0f2b724d6d65a9
Raphael Faria <raphaelf9@outlook.com>
2023-01-08 15:17:33 -0300

    Bean Validation

--- f9-commerce/src/main/java/com/raphaelf9/f9commerce/controllers/ProductController.java
+++ f9-commerce/src/main/java/com/raphaelf9/f9commerce/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.raphaelf9.f9commerce.controllers;
 
 import com.raphaelf9.f9commerce.dto.ProductDTO;
 import com.raphaelf9.f9commerce.services.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -32,7 +33,7 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto)
+    public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto)
     {
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder
@@ -43,7 +44,7 @@ public class ProductController {
     }
 
     @PutMapping (value="/{id}")
-    public ResponseEntity<ProductDTO> update(@PathVariable Long id,@RequestBody ProductDTO dto ){
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id,@Valid @RequestBody ProductDTO dto ){
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
     }
--- f9-commerce/src/main/java/com/raphaelf9/f9commerce/dto/ProductDTO.java
+++ f9-commerce/src/main/java/com/raphaelf9/f9commerce/dto/ProductDTO.java
@@ -1,12 +1,22 @@
 package com.raphaelf9.f9commerce.dto;
 
 import com.raphaelf9.f9commerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 
 public class ProductDTO {
 
     private Long id;
+    @Size(min=3, max=80, message="Nome precisa ter entre 03 e 80 caracteres.")
+    @NotBlank(message="Campo requerido.")
     private String name;
+    @Size(min=10, message="Descrição precisa ter, no mínimo, 10 caracteres.")
+    @NotBlank(message = "Campo requerido.")
     private String description;
+    @Positive(message="O preço deve ser positivo.")
     private Double price;
     private String imgUrl;
 
